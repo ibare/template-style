69d40404c2839cf3e873ad7e32ab5fb3bed0943e
김민태 <kim.mintae@woowahan.com>
2024-06-24 15:51:32 +0900

    init commit

--- src/pages/404/PageNotFound.js
+++ src/pages/404/PageNotFound.js
@@ -1,9 +1,11 @@
 import handlebars from 'handlebars'
 
+const html = handlebars.compile(`
+  <h1>404</h1>
+  <p>{{content}}</p>  
+`)
+
 export default function pageNotFound() {
   
-  return handlebars.compile(`
-    <h1>404</h1>
-    <p>{{content}}</p>
-  `)({ content: '페이지를 찾을 수 없습니다.'})
+  return html({ content: '페이지를 찾을 수 없습니다.'})
 }
